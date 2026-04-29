a4493e66afdef1bbff7da38715ffc392900c5983
inceCheng <chg99925@gmail.com>
2026-04-29 22:36:41 +0800

    169. 多数元素

---
+++ src/main/java/com/chg/hot100/MajorityElement.java
@@ -0,0 +1,22 @@
+package com.chg.hot100;
+
+/**
+ * 169. 多数元素
+ */
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            if (candidate == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
