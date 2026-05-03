3d4860a7b8a41adad7c9c6acdb842223ebb0d504
inceCheng <chg99925@gmail.com>
2026-05-03 23:05:35 +0800

    74. 搜索二维矩阵

--- src/main/java/com/chg/hot100/SearchMatrix.java
+++ src/main/java/com/chg/hot100/SearchMatrix.java
@@ -1,24 +1,22 @@
 package com.chg.hot100;
 
+/**
+ * 74. 搜索二维矩阵
+ */
 public class SearchMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix[0][0] > target || matrix[matrix.length - 1][matrix[0].length - 1] < target) {
             return false;
         }
-        // 从矩阵的左上角开始寻找
-        // 当前值小于 target，则向下找
-        // 当前值大于 target，则向左找
         int i = 0;
         int j = matrix[0].length - 1;
-        int row = matrix.length - 1;
-        while (i <= row && j >= 0) {
+        while (i <= matrix.length - 1 && j >= 0) {
             if (matrix[i][j] == target) {
                 return true;
-            }
-            if (matrix[i][j] > target) {
-                j--;
-            } else {
+            } else if (matrix[i][j] < target) {
                 i++;
+            } else {
+                j--;
             }
         }
         return false;
