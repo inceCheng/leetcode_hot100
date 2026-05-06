c0d88fd569de0557cc277af70cdc6109957e2409
inceCheng <chg99925@gmail.com>
2026-05-06 21:42:53 +0800

    239. 滑动窗口最大值

---
+++ src/main/java/com/chg/hot100/MaxSlidingWindow.java
@@ -0,0 +1,28 @@
+package com.chg.hot100;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 239. 滑动窗口最大值
+ */
+public class MaxSlidingWindow {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            while (!deque.isEmpty() && deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+            while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            if (i >= k - 1) {
+                res[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+        return res;
+    }
+}
