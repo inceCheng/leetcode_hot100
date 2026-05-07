448953ca0580059082449e8f45cada83d62f2002
inceCheng <chg99925@gmail.com>
2026-05-07 15:39:58 +0800

    122. 买卖股票的最佳时机 II

--- src/main/java/com/chg/hot100/MaxProfit.java
+++ src/main/java/com/chg/hot100/MaxProfit.java
@@ -10,16 +10,36 @@ package com.chg.hot100;
  * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
  */
 public class MaxProfit {
+
     // 记录历史最低价
     // 每一天都判断当前天卖出是否是最高价，如果是，则更新结果，如果不是，则继续循环
+    // public int maxProfit(int[] prices) {
+    //     int min = prices[0];
+    //     int res = 0;
+    //     for (int price : prices) {
+    //         if (price < min) {
+    //             min = price;
+    //         } else if (price - min > res) {
+    //             res = price - min;
+    //         }
+    //     }
+    //     return res;
+    // }
+
+    /**
+     * 122. 买卖股票的最佳时机 II
+     *
+     * @param prices
+     * @return
+     */
     public int maxProfit(int[] prices) {
-        int min = prices[0];
         int res = 0;
-        for (int price : prices) {
-            if (price < min) {
-                min = price;
-            } else if (price - min > res) {
-                res = price - min;
+        if (prices == null || prices.length <= 1) {
+            return res;
+        }
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] - prices[i - 1] > 0) {
+                res += prices[i] - prices[i - 1];
             }
         }
         return res;
